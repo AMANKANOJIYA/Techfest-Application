646c5a05a2124edfc3edfc523b835548ec4b5ad4
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2021-12-03 22:59:31 +0530

    Added Callback

--- app.js
+++ app.js
@@ -1,53 +1,77 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 const app = express();
-const appId = process.env.APPID
+const appId = process.env.APPID;
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/views'))
-app.use(cookieParser())
-app.use(session({
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/views"));
+app.use(cookieParser());
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
     cookie: {
-        maxAge: Date.now() + (24 * 60 * 60 * 1000)
+      maxAge: Date.now() + 24 * 60 * 60 * 1000,
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  const state = crypto.randomBytes(12).toString("hex");
+  req.session.state = state;
+  res.redirect(
+    `${process.env.MIC_URL}/appId=${appId}&sensitive=false&state=${state}`
+  );
+});
+
+app.post("/callback", (req, res) => {
+  const token = req.body.token || req.query.token;
+  if (!token) {
+    res.redirect("/");
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    if (decoded.appId !== appId && decoded.state !== req.session.state) {
+      res.redirect("/");
     }
-}))
-
-app.get('/',  (req, res)=> {
-    res.render('home');
-});
-
-app.get('/login', (req, res)=> {
-  res.redirect(`${process.env.MIC_URL}/appId=${appId}&sensitive=true`);
-});
-
-app.post('/callback', (req, res) => {
-    req.session.user = req.body.user;
+    req.session.user = decoded.userInfo;
     req.session.save();
-    res.redirect('/profile')
-})
+  } catch (err) {
+    res.redirect("/");
+  }
+  res.redirect("/profile");
+});
 
-app.get('/profile', (req, res) => {
-    if (req.session.user) {
-        res.render('profile')
+app.get("/callback", (req, res) => {
+  res.redirect("/profile");
+});
+
+app.get("/profile", (req, res) => {
+  if (req.session.user) {
+    res.render("profile");
+  }
+  res.redirect("/");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return console.log(err);
     }
-    res.redirect('/')
-})
-
-app.get('/logout', (req,res) => {
-    req.session.destroy((err) => {
-        if(err) {
-            return console.log(err);
-        }
-        res.redirect('/profile');
-    });
-    res.redirect('/')
-})
+    res.redirect("/profile");
+  });
+  res.redirect(`/`);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
